995bda5fa37659ef6c8135a5f3848c7fa845eecb
samuelebruni <samuele_bruni@hotmail.com>
2023-09-11 14:55:55 +0200

    add bonus

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Grazie a dio esiste vueJS (?)'
+        message: 'Grazie a dio esiste vueJS (?)',
+        image: 'https://fastly.picsum.photos/id/565/300/300.jpg?hmac=0aeSSgdxvnJYQ22Mm2VbUdUVvz8FDG-CH3Xgxd9eoDs'
       }
     }
   }).mount('#app')
